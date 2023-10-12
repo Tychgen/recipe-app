c3b03e6e63559fa03e3ba6bfb5bc8167096894d7
Tychgen <alikxelefov@gmail.com>
2023-10-13 00:39:01 +0400

    Features:Modal added

--- src/Pages/AppModal.jsx
+++ src/Pages/AppModal.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import {Modal} from 'antd'
 
-const AppModal = () => {
+const AppModal = ({isModalOpen,handleOk,handleCancel, currentRecipe }) => {
   return (
-    <div>
-      
-    </div>
+      <Modal title={currentRecipe ? currentRecipe.name : ''} open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        {currentRecipe && (
+          <div>
+            <img
+              src={currentRecipe.url}
+              alt={currentRecipe.name}
+              style={{ width: '100%', height: 'auto' }}
+            />
+            <h2>{currentRecipe.name}</h2>
+            <p>Rating: {currentRecipe.rating}</p>
+            <p>Cooking Time: {currentRecipe.cookingTime}</p>
+            <p>Serves: {currentRecipe.serves} people</p>
+            <h4>Ingredients:</h4>
+            <ul>
+              {currentRecipe.ingredients.map((ingredient, i) => (
+                <li key={i}>{ingredient}</li>
+              ))}
+            </ul>
+            <h4>Instructions:</h4>
+            <p>{currentRecipe.instructions}</p>
+          </div>
+        )}
+      </Modal>
   )
 }
 
